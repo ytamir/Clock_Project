c038ddefa1c769339e9150e9c192598b39a5e12d
Alec Knutsen <aqknutsen@gmail.com>
2016-02-05 11:59:41 -0600

    Add member variable class name and constructors for MainActivity.java and TimerActivity.java for debugging purposes. Add Add methos such as onDestroy, onResume, etc. to both TimerActivity.java and MainActivity.java

--- Clock/src/com/example/clock/MainActivity.java
+++ Clock/src/com/example/clock/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.clock;
 
 import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
+@SuppressLint("NewApi")
 public class MainActivity extends ActionBarActivity {
+	
+	private String class_name;
+	
+	/*
+	 * Constructor the stores the Class_Name for debugging purposes 
+	 */
+	private MainActivity() {
+		
+		class_name = getClass().getName();
+		
+	}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Log.d(class_name, "Creating");
     }
 
     @Override
@@ -32,4 +48,62 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    @Override
+	protected void onStart() {
+		
+		
+		super.onStart(); // Make sure the code in the parent class is used 
+		
+		
+		Log.d(class_name, "Starting");
+		
+    }
+    
+    
+    @Override
+	protected void onResume() {
+		
+		
+		super.onResume(); // Call code of parent class
+		
+		Log.d(class_name, "Resuming");
+		
+	}
+    
+    
+    @Override
+	protected void onPause() {
+		
+		
+		super.onPause(); // Call parent code 
+		
+		Log.d(class_name, "Pausing");
+		
+	}
+    
+    
+    protected void onStop() {
+		
+		
+		super.onStop(); // Call parent code 
+		
+		
+		Log.d(class_name, "Stopping");
+		
+	}
+    
+    
+    @Override
+	protected void onDestroy() {
+		
+		
+		super.onDestroy();
+		
+		Log.d(class_name, "Destroying");
+		
+	}
+    
+    
+		
 }
--- Clock/src/com/example/clock/Settings.java
+++ Clock/src/com/example/clock/Settings.java
@@ -1,11 +1,26 @@
 package com.example.clock;
 
 import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+@SuppressLint("NewApi")
 public class Settings extends ActionBarActivity {
+	
+	
+	private String class_name;
+	
+	/*
+	 * Constructor the stores the Class_Name for debugging purposes 
+	 */
+	private Settings() {
+		
+		class_name = getClass().getName();
+		
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,4 +46,60 @@ public class Settings extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
+	  @Override
+		protected void onStart() {
+			
+			
+			super.onStart(); // Make sure the code in the parent class is used 
+			
+			
+			Log.d(class_name, "Starting");
+			
+	    }
+	    
+	    
+	    @Override
+		protected void onResume() {
+			
+			
+			super.onResume(); // Call code of parent class
+			
+			Log.d(class_name, "Resuming");
+			
+		}
+	    
+	    
+	    @Override
+		protected void onPause() {
+			
+			
+			super.onPause(); // Call parent code 
+			
+			Log.d(class_name, "Pausing");
+			
+		}
+	    
+	    
+	    protected void onStop() {
+			
+			
+			super.onStop(); // Call parent code 
+			
+			
+			Log.d(class_name, "Stopping");
+			
+		}
+	    
+	    
+	    @Override
+		protected void onDestroy() {
+			
+			
+			super.onDestroy();
+			
+			Log.d(class_name, "Destroying");
+			
+		}
 }
